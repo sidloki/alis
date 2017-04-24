7f1f3ba5bf80ae12d6bb9170ce99939f3f9f7330
Marco Strobel <mail@strobel.ch>
2017-04-24 21:50:00 +0200

    update global resources config

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,6 +1,8 @@
 export function configure(config) {
   config.globalResources([
     './elements/ons-back-button',
-    './elements/ons-navigator'
+    './elements/ons-navigator',
+    './elements/ons-tabbar',
+    './elements/ons-select'
   ]);
 }
