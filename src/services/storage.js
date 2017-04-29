09152fcc11b9efdf929f15b73c84b3581a5a2acb
Marco Strobel <mail@strobel.ch>
2017-04-29 10:37:31 +0200

    add storage service

---
+++ src/services/storage.js
@@ -0,0 +1,17 @@
+export class Storage {
+
+  constructor() {
+    this.stores = {
+      local: localStorage,
+      session: sessionStorage
+    };
+  }
+
+  getItem(name, store='local') {
+    return JSON.parse(this.stores[store].getItem(name));
+  }
+
+  setItem(name, value, store='local') {
+    this.stores[store].setItem(name, JSON.stringify(value));
+  }
+}
