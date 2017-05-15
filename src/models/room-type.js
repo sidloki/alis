3091843348dd91de43e893fe489c147ff2e227ba
Marco Strobel <mail@strobel.ch>
2017-05-15 12:07:07 +0200

    add room type icons

--- src/models/room-type.js
+++ src/models/room-type.js
@@ -1,3 +1,16 @@
+const Icons = {
+  1: 'mki-building',
+  2: 'ion-university, material:md-graduation-cap',
+  3: 'mki-chapel',
+  4: 'mki-museum',
+  5: 'mki-court_house',
+  6: 'ion-ios-email, material:md-email',
+  7: 'ion-ios-people',
+  8: 'ion-ios-film, material:md-movie-alt',
+  9: 'mki-theatre'
+};
+
+
 export class RoomType {
 
   constructor(data) {
@@ -10,5 +23,9 @@ export class RoomType {
 
   get id() {
     return this.typID;
+  }
+
+  get icon() {
+    return Icons[this.id];;
   }
 }
